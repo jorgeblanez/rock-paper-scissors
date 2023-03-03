ea000c9322cbb7548e2296bf3369d99ac1dae9e2
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-03-02 22:33:09 -0300

    Create play round function
    The playRound function receives both the player's and the computer's
    choices, and defines a winner. Inside the function we increment either a
    player count, or a computer count, to determine the winner later.
    The return of the function is an alert of the result to the player.

--- script.js
+++ script.js
@@ -21,10 +21,13 @@ function getComputerChoice(){
 function game(){
     playerCount = 0;
     computerCount = 0;
-    for (i=0;i<5;i++){
-
+    let keepGoing =true;
+    while (keepGoing){
         playRound(getPlayerChoice(),getComputerChoice());
+        if (playerCount === 3 || computerCount === 3) keepGoing =false;
     }
+    if (playerCount > computerCount) return alert("You won! :D")
+    else return alert("You lost! :/")
 }
 //Get player selection, which should be case insensitive, and validate if the
 //input was correct. Use "prompt()"
@@ -37,7 +40,7 @@ function getPlayerChoice(){
         if (playerChoice === "rock" || 
             playerChoice === "paper" || 
             playerChoice === "scissors") return playerChoice
-        else alert("Invalid option! try again!")
+        else alert("Invalid option! Try again!")
     }
    
 }
@@ -46,8 +49,42 @@ function getPlayerChoice(){
 //that receive playerSelection and computerSelection
 //and return the result (win/lose/draw + winningSelection beats losingSelection)
 
+function playRound(playerChoice,computerChoice){
+    let result;
+    if (playerChoice === computerChoice){
+        return alert ("Draw! Try again")
+    }
 
-getPlayerChoice();
+    if (playerChoice === "rock" && computerChoice ==="scissors"){
+        playerCount++;
+        result = "You won! Rock beats Scissors!"
+    }
+    else{
+        computerCount++;
+        result = "You lost! Paper beats Rock!"
+    }
+    
+    if (playerChoice === "paper" && computerChoice ==="rock"){
+        playerCount++;
+        result = "You won! Paper beats Rock!"
+    }
+    else{
+        computerCount++;
+        result = "You lost! Scissors beats Paper!"
+    }
+
+    if (playerChoice === "scissors" && computerChoice ==="paper"){
+        playerCount++;
+        result = "You won! Scissors beats Paper!"
+    }
+    else{
+        computerCount++;
+        result = "You lost! Rock beats Scissors!"
+    }
+    return alert(result);
+
+}
+
 let playerCount = 0;
 let computerCount = 0;
 
