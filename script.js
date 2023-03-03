1745a67c4525ee40193281f6bc7aa7fc0d808261
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-03-02 21:59:39 -0300

    Create functions for computer choice, player choice and game
    Three functions were created:
    
    (1) - Game function: This function aims to control the best of 5 game
          between the player and the computer;
    (2) - Computer choice: This function generate a random number from 0 to
          2, and returns the equivalent of the Rock Paper Scissors choice,
          where 0 = Rock, 1 = Paper, and 2 = Scissors. The choices are
          returned as lower case;
    (3) - Player Choice: This function asks the player to type in a choice
          and keeps asking until the choice is either rock, paper or
          scissors. The choice is normalized to normal case as well.

--- script.js
+++ script.js
@@ -1,15 +1,54 @@
-//Create function getComputerChoice that will randomly return text "Rock", "Paper" or "Scissors"
+//Create function getComputerChoice that will randomly return text "rock", 
+//"paper" or "scissors"
+function getComputerChoice(){
+    let choice = Math.floor(Math.random *3);
+    switch (choice) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors"
+            break;
+    }
+}
 
+//Create a function called game. A game should be best of 5 and the function should 
+//report who won at the end.
+
+function game(){
+    playerCount = 0;
+    computerCount = 0;
+    for (i=0;i<5;i++){
+
+        playRound(getPlayerChoice(),getComputerChoice());
+    }
+}
 //Get player selection, which should be case insensitive, and validate if the
 //input was correct. Use "prompt()"
+
+function getPlayerChoice(){
+    keepGoing = true
+    while(keepGoing){
+        let playerChoice = prompt("Type in your choice between Rock, Paper or Scissors")
+
+        if (playerChoice === "rock" || 
+            playerChoice === "paper" || 
+            playerChoice === "scissors") return playerChoice
+        else alert("Invalid option! try again!")
+    }
+   
+}
 
 //Create function to play a round of Rock Paper Scissors, 
 //that receive playerSelection and computerSelection
 //and return the result (win/lose/draw + winningSelection beats losingSelection)
 
-//Create a function called game. A game should be best of 5 and the function should report
-//who won at the end.
 
-
+getPlayerChoice();
+let playerCount = 0;
+let computerCount = 0;
 
 
