3c9849c112eb49f56dcea4fc745ae4c4ca0f8792
Jorge <13945741+jorgeblanez@users.noreply.github.com>
2023-03-02 21:42:08 -0300

    Create pseudocode for the solution

---
+++ script.js
@@ -0,0 +1,15 @@
+//Create function getComputerChoice that will randomly return text "Rock", "Paper" or "Scissors"
+
+//Get player selection, which should be case insensitive, and validate if the
+//input was correct. Use "prompt()"
+
+//Create function to play a round of Rock Paper Scissors, 
+//that receive playerSelection and computerSelection
+//and return the result (win/lose/draw + winningSelection beats losingSelection)
+
+//Create a function called game. A game should be best of 5 and the function should report
+//who won at the end.
+
+
+
+
